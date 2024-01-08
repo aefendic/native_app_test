6f5471933babe09aff44edb48f7c7d0055341d43
Armin Efendic <aefendic@hex.tech>
2024-01-08 13:24:38 -0500

    commit for charles

--- python/weather.py
+++ python/weather.py
@@ -1,21 +1,14 @@
-# reach out to the weather api
-import requests
-import pandas as pd
-from datetime import datetime
-from snowflake.snowpark.context import get_active_session
+# sprocs can only take in one function
+def get_daily_weather(session):
+    import requests
+    import pandas as pd
+    from datetime import datetime
+    from snowflake.snowpark.context import get_active_session
 
-api_key = '8b52bac71e9f6d61ca801afc28e03874'
-lat = 0
-long = 0
-units = "metric"
-
-def main():
-    weather_data = get_daily_weather(api_key, 0, 0, units)
-    if weather_data is not None:
-        print(weather_data)
-        # write_to_snowflake_table(weather_data)
-
-def get_daily_weather(api_key, lat, long, units):
+    api_key = '8b52bac71e9f6d61ca801afc28e03874'
+    lat = 0
+    long = 0
+    units = "metric"
     # OpenWeatherMap API endpoint for daily forecast
     endpoint = f"https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lat}&appid={api_key}&units={units}"
 
@@ -27,28 +20,20 @@ def get_daily_weather(api_key, lat, long, units):
         data = response.json()
 
         # Extract relevant information for each day
-
         today_date = datetime.now().date()
         temperature = data["main"]["temp"]
 
-        # Create a DataFrame from the collected data
+        # Create a DataFrame from the collected data and write to snowflake
         df = pd.DataFrame({'date': [today_date], 'temp': [temperature]})
-        return df
-
+        session = get_active_session()
+        session.write_pandas(df, 'daily_weather', schema='weather', auto_create_table=True, table_type="") # appends data if table exists
+        return "success"
     else:
         print(f"Status code {response.status_code}")
         print(f"Error details: {response.text}")
         return None
 
-def write_to_snowflake_table(df):
-    session = get_active_session()
-    session.write_pandas(df, 'daily_weather', auto_create_table=True, table_type="") # appends data if table exists
-
 # send the written data to snowflakes billing system
-
-if __name__ == "__main__":
-    # Call the main function when the script is run
-    main()
 
 
 
--- scripts/setup.sql
+++ scripts/setup.sql
@@ -1,17 +1,53 @@
+-- ==========================================
+-- This script runs when the app is installed 
+-- ==========================================
+
 -- Create role and schema contained within the application only (does not create the role in your account)
+
 CREATE APPLICATION ROLE app_public;
-CREATE SCHEMA IF NOT EXISTS core;
-GRANT USAGE ON SCHEMA core TO APPLICATION ROLE app_public;
+CREATE SCHEMA IF NOT EXISTS weather;
+GRANT USAGE ON SCHEMA weather TO APPLICATION ROLE app_public;
+
+CREATE OR ALTER VERSIONED SCHEMA versioned_schema;
+GRANT USAGE ON SCHEMA versioned_schema TO APPLICATION ROLE app_public;
+
+-- Create UDF (if using imports=() need to use a versioned schema)
+create PROCEDURE versioned_schema.WEATHER()
+returns TABLE (DATE DATE, TEMP BIGINT)
+language python
+runtime_version = '3.8'
+packages = ('snowflake-snowpark-python', 'requests', 'pandas')
+imports = ('/python/weather.py')
+handler = 'weather.get_daily_weather';
+
+GRANT USAGE ON PROCEDURE versioned_schema.weather() TO APPLICATION ROLE app_public;
+
+-- streamlit object
+CREATE STREAMLIT versioned_schema.hello_snowflake_streamlit
+  FROM '/streamlit'
+  MAIN_FILE = '/display_weather.py';
+GRANT USAGE ON STREAMLIT versioned_schema.hello_snowflake_streamlit TO APPLICATION ROLE app_public;
 
 
-CREATE OR REPLACE PROCEDURE CORE.WEATHER()
-  RETURNS TABLE (DATE DATE, TEMP BIGINT)
-  LANGUAGE PYTHON
-  EXECUTE AS OWNER
-  RUNTIME_VERSION = "3.9"
-  IMPORTS = ('/python/weather.py')
-  HANDLER='hello_python.main';
+-- Callback Stored Procedure for the External Access Integraion reference
+-- CREATE PROCEDURE versioned_schema.REGISTER_SINGLE_REFERENCE(ref_name STRING, operation STRING, ref_or_alias STRING)
+--   RETURNS STRING
+--   LANGUAGE SQL
+--   AS $$
+--     BEGIN
+--       CASE (operation)
+--         WHEN 'ADD' THEN
+--           SELECT SYSTEM$SET_REFERENCE(:ref_name, :ref_or_alias);
+--         WHEN 'REMOVE' THEN
+--           SELECT SYSTEM$REMOVE_REFERENCE(:ref_name);
+--         WHEN 'CLEAR' THEN
+--           SELECT SYSTEM$REMOVE_REFERENCE(:ref_name);
+--       ELSE
+--         RETURN 'unknown operation: ' || operation;
+--       END CASE;
+--       RETURN NULL;
+--     END;
+--   $$;
 
-GRANT USAGE ON PROCEDURE core.weather() TO APPLICATION ROLE app_public;
-
-
+-- GRANT USAGE ON PROCEDURE versioned_schema.REGISTER_SINGLE_REFERENCE(STRING, STRING, STRING)
+--   TO APPLICATION ROLE app_public;
